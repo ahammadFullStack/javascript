46b3d816de2cb06437005225c1cec5738bc983ba
ahammadFullStack <ahammad76718@gmail.com>
2021-01-05 20:04:18 +0530

    giii

---
+++ digit.js
@@ -0,0 +1,19 @@
+var n=4874;
+var count=0;
+
+   for(var i=1;i<=n;i++)
+   {
+
+    count++;
+    n/=10;
+   }
+   
+
+if(count==3)
+{
+    console.log("3 digit");
+}
+else
+{
+    console.log("not 3 digit");
+}
